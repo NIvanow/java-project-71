9b2c9574823fd798e788396666b9fb0859024863
Nikita Ivanov <ivanow.1994@gmail.com>
2024-09-18 21:07:05 +0500

    added Hello World!

---
+++ build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    id("java")
+    application
+    id("checkstyle")
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.3"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+application {
+    mainClass.set("hexlet.code.App")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
