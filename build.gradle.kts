dd7055e48131a5ebf8a4aaead0f7079ad15d6bfa
Nikita Ivanov <ivanow.1994@gmail.com>
2024-10-07 21:47:41 +0500

    added Picocli

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,11 @@ plugins {
     id("java")
     application
     id("checkstyle")
+    id ("org.graalvm.buildtools.native") version "0.10.2"
+    distribution
+
 }
+
 
 group = "hexlet.code"
 version = "1.0-SNAPSHOT"
@@ -14,7 +18,12 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.3"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("info.picocli:picocli:4.7.6")
+    compileOnly ("info.picocli:picocli-codegen:4.7.6")
+    annotationProcessor ("info.picocli:picocli-codegen:4.7.6")
 }
+
+
 
 application {
     mainClass.set("hexlet.code.App")
@@ -22,4 +31,5 @@ application {
 
 tasks.test {
     useJUnitPlatform()
+
 }
--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -1,7 +1,30 @@
 package hexlet.code;
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+
+import java.util.concurrent.Callable;
+
+
+@Command(name = "gendiff", mixinStandardHelpOptions = true, version = "version 1.0",
+        description = "Compares two configuration files and shows a difference")
+class GenDiff implements Callable<Integer> {
+
+    @Override
+    public Integer call()  { // your business logic goes here...
+        return 0;
+    }
+
+    // this example implements Callable, so parsing, error handling and handling user
+    // requests for usage help or version help can be done with one line of code.
+   // public static void main(String... args) {
+
+    //}
+}
+
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int exitCode = new CommandLine(new GenDiff()).execute(args);
+        System.exit(exitCode);
     }
 }
