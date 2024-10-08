9432705ec48b4625c44817dbd5efebd8c3c8bfd8
Nikita Ivanov <ivanow.1994@gmail.com>
2024-10-08 22:00:46 +0500

    added option "format"

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -1,9 +1,11 @@
 package hexlet.code;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
 
 import java.util.concurrent.Callable;
-
+import java.io.File;
 
 @Command(name = "gendiff", mixinStandardHelpOptions = true, version = "version 1.0",
         description = "Compares two configuration files and shows a difference")
@@ -13,6 +15,17 @@ class GenDiff implements Callable<Integer> {
     public Integer call()  { // your business logic goes here...
         return 0;
     }
+    @Option(names = {"-f", "--format"}, paramLabel = "format", description = "output format [default: stylish]")
+    String format ;
+
+    @Parameters(index = "0", paramLabel = "filepath1", description = "path to first file")
+    private File file1;
+
+    @Parameters(index = "0", paramLabel = "filepath2", description = "path to second file")
+    private File file2;
+
+    /*@Option(names = {"-h", "--help"}, usageHelp = true, description = "display a help message")
+    private boolean helpRequested = false;*/
 
     // this example implements Callable, so parsing, error handling and handling user
     // requests for usage help or version help can be done with one line of code.
